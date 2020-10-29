0fb29a6ec18b37017f787710a9072eb0c94063d4
Sergey Kaplan <sergeykpn@gmail.com>
2020-10-30 01:56:38 +0200

    Lesha assignment

--- src/Card.js
+++ src/Card.js
@@ -25,40 +25,49 @@ const Card = ({
   link
 }) => {
     const discount = Math.round(100 - ((final_price / price) * 100));
+    const slides = [
+        {alt:'front', src:img_front},
+        {alt:'side', src:img_side},
+        {alt:'angle', src:img_angle},
+    ]
     return (
-<Col md={4} sm={12} >
-    <div className="card">
-        <div className="top-line">
-            <label className="discount">{discount}% OFF</label>
-            <label className="count">
-                {qty} Items left
-                <FaGripfire size={24} spacing={5}/>
-            </label>
-        </div>
-        <Carousel interval={null}>
-            <Carousel.Item>
-                <img src={img_front} className="d-block w-100" alt="front" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={img_angle} className="d-block w-100" alt="angle"/>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={img_side} className="d-block w-100" alt="side"/>
-            </Carousel.Item>
-        </Carousel>
-        <h1 className="name">{name}</h1>
-        <p className="text-center">
-            <strike>${price}</strike>
-            <strong className="final-price">${final_price}</strong>
-            <span> + Free Shipping</span>
-        </p>
+        <Col md={4} sm={12} >
+            <div className="card">
+                <div className="top-line">
+                    <label className="discount">{discount}% OFF</label>
+                    <label className="count">
+                        {qty} Items left
+                        <FaGripfire size={24} spacing={5}/>
+                    </label>
+                </div>
+                <Carousel interval={null}>
+                    {slides.map(item => {
+                        return (
+                            <Carousel.Item
+                                key={item.src}
+                            >
+                                <img
+                                    className="d-block w-100"
+                                    src={item.src}
+                                    alt={item.alt}
+                                />
+                            </Carousel.Item>
+                        );
+                    })}
+                </Carousel>
+                <h1 className="name">{name}</h1>
+                <p className="text-center">
+                    <strike>${price}</strike>
+                    <strong className="final-price">${final_price}</strong>
+                    <span> + Free Shipping</span>
+                </p>
 
-        {
-            !!qty ? <ShopBtn /> : <SoldBtn/>
-        }
-    </div>
+                {
+                    !!qty ? <ShopBtn link={link}/> : <SoldBtn/>
+                }
+            </div>
 
-</Col>
+        </Col>
     );
 };
 
